f14d1689d8f1ba445db0c5f45b7d5ff35f42e71b
cahitYes <efeyesiloglu@hotmail.com>
2021-10-21 15:36:40 +0200

    test

--- 01-phpinfo.php
+++ 01-phpinfo.php
@@ -2,4 +2,4 @@
 
 // fonction native de PHP, nous affiche les versions, bibliothèques et réglages de PHP (utile au changement de serveur)
 // https://www.php.net/manual/fr/function.phpinfo
-phpinfo();
+phpinfo();  
