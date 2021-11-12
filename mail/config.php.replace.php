1b3660383152430a123023fff94f64f1108660fa
cahitYes <efeyesiloglu@hotmail.com>
2021-11-12 09:21:01 +0100

    config.php.replace

---
+++ mail/config.php.replace.php
@@ -0,0 +1,7 @@
+// pour l envoie d'un mail
+
+// SMTP (simple mail transfert protocol) protocole d'envoie de mail 
+define("SMTP_HOST", "relay.skynet.be");
+
+// Mail de l'admin 
+define("MAIL_ADMIN",cahit.yesiloglu@cf2m.onmicrosoft.be");
