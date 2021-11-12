48ee7d0d4d017dff3600e34f04f34c892c8942ac
mikhawa <michaeljpitz@gmail.com>
2021-11-12 09:29:16 +0100

    port and smtp

--- 23-mail/config.php.replace.php
+++ 23-mail/config.php.replace.php
@@ -3,5 +3,8 @@
 // SMTP (protocole d'envoi de mail - Simple Mail Transfer Protocol)
 define("SMTP_HOST","localhost");
 
+// Port 
+define("SMTP_PORT",25);
+
 // Mail de l'admin 
 define("MAIL_ADMIN","lulu@lulu.com");
