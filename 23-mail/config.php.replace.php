72c1bc2b37c1c60fc95d4a44c4d0beda0ee19d61
mikhawa <michaeljpitz@gmail.com>
2021-11-12 09:21:12 +0100

    config.php.replace

---
+++ 23-mail/config.php.replace.php
@@ -0,0 +1,7 @@
+// pour l'envoi d'un mail
+
+// SMTP (protocole d'envoi de mail - Simple Mail Transfer Protocol)
+define("SMTP_HOST","localhost");
+
+// Mail de l'admin 
+define("MAIL_ADMIN","lulu@lulu.com");
